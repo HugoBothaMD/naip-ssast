80cd75c1746a79ecd3f9764d73688f878c0c97a5
dwiepert <81657496+dwiepert@users.noreply.github.com>
2023-05-22 11:46:43 -0500

    default args

--- src/run.py
+++ src/run.py
@@ -290,24 +290,24 @@ def main():
     #Inputs
     parser.add_argument('-i','--prefix',default='speech_ai/speech_lake/', help='Input directory or location in google cloud storage bucket containing files to load')
     parser.add_argument("-s", "--study", choices = ['r01_prelim','speech_poc_freeze_1', None], default='speech_poc_freeze_1', help="specify study name")
-    parser.add_argument("-d", "--data_split_root", default='gs://ml-e107-phi-shared-aif-us-p/speech_ai/share/data_splits/amr_subject_dedup_594_train_100_test_binarized_v20220620/test.csv', help="specify file path where datasplit is located. If you give a full file path to classification, an error will be thrown. On the other hand, evaluation and embedding expects a single .csv file.")
-    parser.add_argument('-l','--label_txt', default=None)#'/Users/m144443/Documents/GitHub/mayo-ssast/src/labels.txt')
+    parser.add_argument("-d", "--data_split_root", default='gs://ml-e107-phi-shared-aif-us-p/speech_ai/share/data_splits/amr_subject_dedup_594_train_100_test_binarized_v20220620', help="specify file path where datasplit is located. If you give a full file path to classification, an error will be thrown. On the other hand, evaluation and embedding expects a single .csv file.")
+    parser.add_argument('-l','--label_txt', default='src/labels.txt')
     parser.add_argument('--lib', default=False, type=bool, help="Specify whether to load using librosa as compared to torch audio")
-    parser.add_argument("--pretrained_mdl_path", type=str, default='/Users/m144443/Documents/mayo_ssast/pretrained_model/SSAST-Base-Frame-400.pth', help="the ssl pretrained models path")#, default='/Users/m144443/Documents/mayo_ssast/pretrained_model/SSAST-Base-Frame-400.pth',) #/Users/m144443/Documents/mayo_ssast/pretrained_model/SSAST-Base-Frame-400.pth
-    parser.add_argument("--finetuned_mdl_path", type=str, default='/Users/m144443/Documents/GitHub/mayo-ssast/debug_exp/amr_subject_dedup_594_train_100_test_binarized_v20220620_base_13_adam_epoch1_ast_pt_mdl.pt', help="if loading an already pre-trained/fine-tuned model")
+    parser.add_argument("--pretrained_mdl_path", type=str, default='/Users/m144443/Documents/GitHub/mayo-ssast/pretrained_model/SSAST-Base-Frame-400.pth', help="the ssl pretrained models path")#, default='/Users/m144443/Documents/mayo_ssast/pretrained_model/SSAST-Base-Frame-400.pth',) #/Users/m144443/Documents/mayo_ssast/pretrained_model/SSAST-Base-Frame-400.pth
+    parser.add_argument("--finetuned_mdl_path", type=str, default=None, help="if loading an already pre-trained/fine-tuned model")
     #GCS
     parser.add_argument('-b','--bucket_name', default='ml-e107-phi-shared-aif-us-p', help="google cloud storage bucket name")
     parser.add_argument('-p','--project_name', default='ml-mps-aif-afdgpet01-p-6827', help='google cloud platform project name')
     parser.add_argument('--cloud', default=False, type=bool, help="Specify whether to save everything to cloud")
     #output
     parser.add_argument("--dataset", default=None,type=str, help="When saving, the dataset arg is used to set file names. If you do not specify, it will assume the lowest directory from data_split_root")
-    parser.add_argument("-o", "--exp_dir", default="./debug_exp/embed", help='specify LOCAL output directory')
-    parser.add_argument('--cloud_dir', default='m144443/temp_out/ssast_orig', type=str, help="if saving to the cloud, you can specify a specific place to save to in the CLOUD bucket")
+    parser.add_argument("-o", "--exp_dir", default="./experiments", help='specify LOCAL output directory')
+    parser.add_argument('--cloud_dir', default='', type=str, help="if saving to the cloud, you can specify a specific place to save to in the CLOUD bucket")
     #Mode specific
-    parser.add_argument("-m", "--mode", choices=['train','eval','extraction'], default='extraction')
+    parser.add_argument("-m", "--mode", choices=['train','eval','extraction'], default='train')
     parser.add_argument("--task", type=str, default='ft_cls', help="pretraining or fine-tuning task", choices=["ft_avgtok", "ft_cls", "pretrain_mpc", "pretrain_mpg", "pretrain_joint"])
     parser.add_argument("--freeze",type=bool, default=True, help="Specify whether to freeze original model before fine-tuning")
-    parser.add_argument("--weighted",type=bool, default=True, help="Specify whether to train the weight sum of layers")
+    parser.add_argument("--weighted",type=bool, default=False, help="Specify whether to train the weight sum of layers")
     parser.add_argument("--layer",type=int, default=-1, help="Specify which model layer output to use. Default is -1 which is the final layer.")
     parser.add_argument('--embedding_type', type=str, default='ft', help='specify whether embeddings should be extracted from classification head (ft) or base pretrained model (pt)', choices=['ft','pt'])
     #Audio configuration parameters
@@ -337,11 +337,11 @@ def main():
     parser.add_argument("--tshape", type=int, default=2, help="shape of patch on the time dimension")
     parser.add_argument('--model_size', default='base',help='the size of AST models', type=str)
     #Training parameters
-    parser.add_argument('--batch_size', default=2, type=int, metavar='N', help='mini-batch size')
+    parser.add_argument('--batch_size', default=8, type=int, metavar='N', help='mini-batch size')
     parser.add_argument('--num_workers', default=0, type=int, metavar='NW', help='# of workers for dataloading (default: 32)')
     parser.add_argument("--epochs", type=int, default=1, help="number of maximum training epochs")
     parser.add_argument("--loss", type=str, default="BCE", help="the loss function for finetuning, depend on the task", choices=["BCE", "CE"])
-    parser.add_argument("--optim", type=str, default="adam", help="training optimizer", choices=["adamw", "adam"])
+    parser.add_argument("--optim", type=str, default="adamw", help="training optimizer", choices=["adamw", "adam"])
     parser.add_argument('-lr', '--learning_rate', default=0.001, type=float, metavar='LR', help='initial learning rate')
     parser.add_argument("--weight_decay", type=float, default=.0001, help='specify weight decay for adamw')
     parser.add_argument("--scheduler", type=str, default=None, help="specify lr scheduler", choices=["onecycle", None])
